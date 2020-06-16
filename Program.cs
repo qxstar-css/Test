fdbcdcdd772c17184453ad2b0bd1b5df8572e936
css <css@qq.com>
2020-06-17 02:28:45 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 30;
+            int b = 20;
+            b = a;
+            a = 10;
+            Console.WriteLine(a);
+            Console.WriteLine(a / b);
+        }
+    }
+}
